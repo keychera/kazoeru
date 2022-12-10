806654ea8555c815a55e981e9471898cf9efa1b5
keychera <kyogatama@gmail.com>
2022-12-11 00:05:21 +0700

    skeleton

--- src/chera/kazoeru.cljs
+++ src/chera/kazoeru.cljs
@@ -1,10 +1,28 @@
 (ns chera.kazoeru
   (:require
    [goog.dom :as gdom]
-   [reagent.dom :as rdom]))
+   [reagent.dom :as rdom]
+   [reagent.core :as r]))
+
+(defonce state (r/atom {:number "" :counter "匹"}))
+
+(defn change-state [key val] (swap! state #(-> % (assoc key (-> val .-target .-value)))))
 
 (defn kazoeru []
-  [:h1 "数えましょう！"])
+  (let [{:keys [number counter] :as current-state} @state]
+    [:div [:h1 "数えましょう！"]
+     [:div
+      [:input {:type "text"
+               :value (str number)
+               :placeholder "何匹？"
+               :on-change #(change-state :number %)}]
+      [:button {:class "w-6 h-6 px-1.5"}
+       [:img {:class "h-4" :src "./img/xmark.svg" :alt "clear-text"}]]
+      [:button {:class "w-6 h-6"}
+       [:svg {:xmlns "http://www.w3.org/2000/svg" :width "24" :height "24" :stroke "#000" :font-size "20" :overflow "visible"}
+                [:text {:font-family "Roboto Condensed" :x "0" :y "24"} counter]]]]
+     [:h2 {} current-state]
+     ]))
 
 ;; the Edge
 (defn get-app-element []
