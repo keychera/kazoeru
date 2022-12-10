da209aa11526a63e4698440f9b92b34cae663b24
keychera <kyogatama@gmail.com>
2022-12-11 00:47:13 +0700

    add num converter from playground

--- src/chera/kazoeru.cljs
+++ src/chera/kazoeru.cljs
@@ -1,15 +1,16 @@
 (ns chera.kazoeru
-  (:require
-   [goog.dom :as gdom]
-   [reagent.dom :as rdom]
-   [reagent.core :as r]))
+  (:require [chera.num-converter :refer [west-arabic->japanese]]
+            [goog.dom :as gdom]
+            [reagent.core :as r]
+            [reagent.dom :as rdom]))
 
 (defonce state (r/atom {:number "" :counter "匹"}))
 
 (defn change-state [key val] (swap! state #(-> % (assoc key (-> val .-target .-value)))))
 
 (defn kazoeru []
-  (let [{:keys [number counter] :as current-state} @state]
+  (let [{:keys [number counter] :as current-state} @state
+        jp-number (or (west-arabic->japanese number) "invalid")]
     [:div {:class "h-screen p-6 bg-neutral-900"}
      [:h1  {:class "mb-0 text-xl text-slate-100"} "数えましょう！"]
      [:h2 {:class "mb-2 text-xs text-neutral-400"} "これどうやっていうの？"]
@@ -26,7 +27,7 @@
        [:svg {:class "fill-slate-100" :xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 24 24" :font-size "24" :overflow "visible"}
         [:text {:x "50%" :y "55%"
                 :dominant-baseline "middle" :text-anchor "middle"} counter]]]]
-     [:h2 {:class "text-lg text-slate-100"} current-state]]))
+     [:h2 {:class "text-lg text-slate-100"} jp-number]]))
 
 ;; the Edge
 (defn get-app-element []
---
+++ src/chera/num_converter.cljs
@@ -0,0 +1,45 @@
+(ns chera.num-converter)
+
+;; number conversion based on data/code from several sources
+;; currently only integer from 0 to 10e4 - 1, haven't made sure every exception yet
+;; -> main source for now: https://www.tofugu.com/japanese/counting-in-japanese/
+;; -> for cross checking some logic: https://github.com/Greatdane/Convert-Numbers-to-Japanese/blob/master/Convert-Numbers-to-Japanese.py
+
+; naming could use some work, basically juu -> digit 0-9 (zero is nil), e -> 10eN , scientific notation symbol
+(def juu-map {\1 "いち"
+              \2 "に"
+              \3 "さん"
+              \4 "よん"
+              \5 "ご"
+              \6 "ろく"
+              \7 "なな"
+              \8 "はち"
+              \9 "きゅう"})
+
+(def e-map {0 #(juu-map %)
+            1 #(case %
+                 \0 nil
+                 \1 "じゅう"
+                 (str (juu-map %) "じゅう"))
+            2 #(case %
+                 \0 nil
+                 \1 "ひゃく"
+                 \3 "さんびゃく"
+                 \6 "ろっぴゃく"
+                 \8 "はっぴゃく"
+                 (str (juu-map %) "ひゃく"))
+            3 #(case %
+                 \0 nil
+                 \1 "せん"
+                 \3 "さんぜん"
+                 \8 "はっせん"
+                 (str (juu-map %) "せん"))
+            4 #(str (juu-map %) "まん")})
+
+;; the name of numeral system refers to this https://en.wikipedia.org/wiki/List_of_numeral_systems
+(defn west-arabic->japanese [num]
+  (as-> (str num) it
+    (reverse it) (map-indexed vector it)
+    (map (fn [[e juu]] (apply (e-map e) [juu])) it)
+    (reduce #(str %2 %1) it)
+    (if (nil? it) "ゼロ" it))) ;; TODO better exceptions handling later
